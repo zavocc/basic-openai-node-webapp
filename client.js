8bb329483a9493f458ac20f43bafe32eb3fb628a
zavocc <zavocc1030@outlook.com>
2024-08-11 20:25:19 +0800

    cool loading animation and disable during processing step

--- client.js
+++ client.js
@@ -27,6 +27,20 @@ async function fetch_response(prompt, model = null) {
     return data.completion
 }
 
+function disable_controls() {
+    document.getElementById('sendbtn').setAttribute("disabled", "disabled");
+    document.getElementById('clearbtn').setAttribute("disabled", "disabled");
+    document.getElementById('inputPrompt').setAttribute("disabled", "disabled");
+    document.getElementById('promptbox').setAttribute("class", "control mt-5 ml-4 mr-4 is-loading");
+}
+
+function enable_controls() {
+    document.getElementById('sendbtn').removeAttribute("disabled");
+    document.getElementById('clearbtn').removeAttribute("disabled");
+    document.getElementById('inputPrompt').removeAttribute("disabled");
+    document.getElementById('promptbox').setAttribute("class", "control mt-5 ml-4 mr-4");
+}
+
 // On click event
 document.addEventListener('DOMContentLoaded', () => {
     // On click event
@@ -34,25 +48,31 @@ document.addEventListener('DOMContentLoaded', () => {
         const prompt = document.getElementById('inputPrompt').value;
         const model = document.getElementById('model').value; 
 
-        let response
-        if (prompt == "/help") {
-            response = "Hello, I am GPT-4, your AI powered answer engine designed to handle basic to advanced chat tasks!\n" +
+        // Disable controls
+        disable_controls();
+        
+        let response;
+        try {
+            if (prompt == "/help") {
+                response = "Hello, I am GPT-4, your AI powered answer engine designed to handle basic to advanced chat tasks!\n" +
 
-            "Here are some things to get you started:\n" +
-            "- Ask real-time questions\n" +
-            "- Solve math problems\n" +
-            "- Execute commands\n" +
+                "Here are some things to get you started:\n" +
+                "- Ask real-time questions\n" +
+                "- Solve math problems\n" +
+                "- Execute commands\n" +
 
-            "\nThose are just a few examples of what I can do. Feel free to ask me anything!\n"
-        } 
-        else 
-        {
-            try{
+                "\nThose are just a few examples of what I can do. Feel free to ask me anything!\n"
+            } 
+                else 
+            {
                 response = await fetch_response(prompt, model);
             }
-            catch (error) {
-                response = error.message;
-            }
+        }
+        catch (error) {
+            response = error.message;
+        } 
+        finally {
+            enable_controls();
         }
 
         // Get the div id; chatbox where the response data will be displayed
